1be8d535e209dfc05928746ac380ec30cf34c6ad
GerwinP <gerwinputtenstein@gmail.com>
2014-04-07 13:43:26 +0200

    Added ConnectGUI(not working yet)

---
+++ Intergrated Project/src/gui/ConnectGUI.java
@@ -0,0 +1,29 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * 
+ */
+public class ConnectGUI extends JPanel implements ActionListener{
+	
+//--------------------------------------------------------------------//
+	
+	private JTextField nickName;
+	private JButton createProfile;
+	private JTextField connAddr;
+	private JButton connect;
+	private static final String
+
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
