583cebeb5c144506d0d9080af79287bf876e3add
Thommie94boy <t.raaijen@live.nl>
2014-04-09 11:39:43 +0200

    please iemand naar background kijken

--- Intergrated Project/src/gui/StartGUI.java
+++ Intergrated Project/src/gui/StartGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -39,6 +40,7 @@ public class StartGUI extends JPanel implements ActionListener {
 		starter.add(northTitle(), BorderLayout.NORTH);
 		starter.add(centerSQ(), BorderLayout.CENTER);
 		starter.add(names(), BorderLayout.SOUTH);
+		
 		starter.setVisible(true);
 		starter.setSize(400, 300);
 		starter.setLocationRelativeTo(null);
@@ -97,6 +99,7 @@ public class StartGUI extends JPanel implements ActionListener {
 	
 	public static void main(String[] args0) {
 		new StartGUI();
+		
 	}
 
 }
