37e1b172db8b7939c1e396d6ddf8de7fd689999b
Thommie94boy <t.raaijen@live.nl>
2014-04-11 11:56:27 +0200

    MaakProfielGUI background etc

--- Intergrated Project/src/gui/MaakProfielGUI.java
+++ Intergrated Project/src/gui/MaakProfielGUI.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,13 +19,16 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-public class MaakProfielGUI implements ActionListener{
+public class MaakProfielGUI implements ActionListener {
+	
+	
 	WindowListener listener = new WindowAdapter(){
 		public void windowClosing(WindowEvent we){
 			maakProfielFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			new ConnectGUI("Nickname");
 		}	
 	};
+	
 	JButton maakProfielButton;
 	JButton geenNieuwProfiel;
 	JFrame maakProfielFrame;
@@ -44,24 +48,42 @@ public class MaakProfielGUI implements ActionListener{
 		grid.setRows(8);
 		grid.setColumns(1);
 		JPanel labelPanel = new JPanel(grid);
+		labelPanel.setBackground(Color.DARK_GRAY);
 		
 		JPanel labelPanel1 = new JPanel();
+		labelPanel1.setBackground(Color.GRAY);
 		JPanel labelPanel2 = new JPanel();
+		labelPanel2.setBackground(Color.GRAY);
 		JPanel labelPanel3 = new JPanel();
+		labelPanel3.setBackground(Color.GRAY);
 		JPanel labelPanel4 = new JPanel();
+		labelPanel4.setBackground(Color.GRAY);
 		JPanel labelPanel5 = new JPanel();
+		labelPanel5.setBackground(Color.GRAY);
 		JPanel labelPanel6 = new JPanel();
+		labelPanel6.setBackground(Color.GRAY);
 		JPanel labelPanel8 = new JPanel();
+		labelPanel8.setBackground(Color.GRAY);
 		JPanel labelPanel9 = new JPanel();
+		labelPanel9.setBackground(Color.GRAY);
 		
-		JLabel nicknameLabel = new JLabel("Nickname*: ");
-		JLabel voornaamLabel = new JLabel("Voornaam*: ");
-		JLabel achternaamLabel = new JLabel("Achternaam*: ");
-		JLabel leeftijdLabel = new JLabel("Leeftijd*: ");
-		JLabel interessesLabel = new JLabel("Interesses: ");
-		JLabel relatiestatusLabel = new JLabel("relatiestatus: ");
-		JLabel passwordLabel = new JLabel("Wachtwoord*: ");
-		JLabel herhaalPasswordLabel = new JLabel("Herhaal wachtwoord*: ");
+		JLabel nicknameLabel = new JLabel("<html> <font color='black'>Nickname*: </font></html>");
+		Font labelsFont = new Font("Bauhaus 93",Font.PLAIN,14);
+		nicknameLabel.setFont(labelsFont);
+		JLabel voornaamLabel = new JLabel("<html> <font color='black'>Voornaam*: </font></html>");
+		voornaamLabel.setFont(labelsFont);
+		JLabel achternaamLabel = new JLabel("<html> <font color='black'>Achternaam*: </font></html>");
+		achternaamLabel.setFont(labelsFont);
+		JLabel leeftijdLabel = new JLabel("<html> <font color='black'>Leeftijd*: </font></html>");
+		leeftijdLabel.setFont(labelsFont);
+		JLabel interessesLabel = new JLabel("<html> <font color='black'>Interesses: </font></html>");
+		interessesLabel.setFont(labelsFont);
+		JLabel relatiestatusLabel = new JLabel("<html> <font color='black'>Relatie status: </font></html>");
+		relatiestatusLabel.setFont(labelsFont);
+		JLabel passwordLabel = new JLabel("<html> <font color='black'>Wachtwoord*: </font></html>");
+		passwordLabel.setFont(labelsFont);
+		JLabel herhaalPasswordLabel = new JLabel("<html> <font color='black'>Herhaal wachtwoord*: </font></html>");
+		herhaalPasswordLabel.setFont(labelsFont);
 		
 		labelPanel1.add(voornaamLabel);
 		labelPanel2.add(achternaamLabel);
@@ -101,13 +123,29 @@ public class MaakProfielGUI implements ActionListener{
 		JPanel textFieldPanel8 = new JPanel();
 		
 		nickname = new JTextField("", 15);
+		nickname.setBackground(Color.LIGHT_GRAY);
+		nickname.setForeground(Color.BLACK);
 		password = new JPasswordField("", 15);
+		password.setBackground(Color.LIGHT_GRAY);
+		password.setForeground(Color.BLACK);
 		herhaalPassword = new JPasswordField("", 15);
+		herhaalPassword.setBackground(Color.LIGHT_GRAY);
+		herhaalPassword.setForeground(Color.BLACK);
 		voornaam = new JTextField("", 15);
+		voornaam.setBackground(Color.LIGHT_GRAY);
+		voornaam.setForeground(Color.BLACK);
 		achternaam = new JTextField("", 15);
+		achternaam.setBackground(Color.LIGHT_GRAY);
+		achternaam.setForeground(Color.BLACK);
 		leeftijd = new JTextField("", 15);
+		leeftijd.setBackground(Color.LIGHT_GRAY);
+		leeftijd.setForeground(Color.BLACK);
 		JTextField interesses = new JTextField(" ", 15);
+		interesses.setBackground(Color.LIGHT_GRAY);
+		interesses.setForeground(Color.BLACK);
 		JTextField relatiestatus = new JTextField(" ", 15);
+		relatiestatus.setBackground(Color.LIGHT_GRAY);
+		relatiestatus.setForeground(Color.BLACK);
 		nickname.setEditable(true);
 		voornaam.setEditable(true);
 		achternaam.setEditable(true);
@@ -116,13 +154,21 @@ public class MaakProfielGUI implements ActionListener{
 		relatiestatus.setEditable(true);
 		
 		textFieldPanel1.add(voornaam);
+		textFieldPanel1.setBackground(Color.GRAY);
 		textFieldPanel2.add(achternaam);
+		textFieldPanel2.setBackground(Color.GRAY);
 		textFieldPanel3.add(leeftijd);
+		textFieldPanel3.setBackground(Color.GRAY);
 		textFieldPanel4.add(interesses);
+		textFieldPanel4.setBackground(Color.GRAY);
 		textFieldPanel5.add(relatiestatus);
+		textFieldPanel5.setBackground(Color.GRAY);
 		textFieldPanel6.add(nickname);
+		textFieldPanel6.setBackground(Color.GRAY);
 		textFieldPanel7.add(password);
+		textFieldPanel7.setBackground(Color.GRAY);
 		textFieldPanel8.add(herhaalPassword);
+		textFieldPanel8.setBackground(Color.GRAY);
 		
 		textFieldPanel.add(textFieldPanel6);
 		textFieldPanel.add(textFieldPanel7);
@@ -138,12 +184,23 @@ public class MaakProfielGUI implements ActionListener{
 	
 	public JPanel maakProfielButtonPanel(){
 		JPanel labelPanel7 = new JPanel();
-		geenNieuwProfiel = new JButton("Ga terug!");
-		maakProfielButton = new JButton("Maak Profiel");
+		labelPanel7.setBackground(Color.GRAY);
+		geenNieuwProfiel = new JButton("<html> <font color='black'>Ga terug!</font></html>");
+		Font buttonFont = new Font("Bauhaus 93",Font.PLAIN,18);
+		geenNieuwProfiel.setFont(buttonFont);
+		geenNieuwProfiel.setOpaque(false);
+		geenNieuwProfiel.setContentAreaFilled(false);
+		geenNieuwProfiel.setBorderPainted(false);
+		maakProfielButton = new JButton("<html> <font color='black'>Maak Profiel!</font></html>");
+		maakProfielButton.setFont(buttonFont);
+		maakProfielButton.setOpaque(false);
+		maakProfielButton.setContentAreaFilled(false);
+		maakProfielButton.setBorderPainted(false);
 		//maakProfielButton.setEnabled(false);
 		maakProfielButton.addActionListener(this);
 		geenNieuwProfiel.addActionListener(this);
-		JPanel maakProfielButtonPanel = new JPanel();	
+		JPanel maakProfielButtonPanel = new JPanel();
+		maakProfielButtonPanel.setBackground(Color.GRAY);
 		maakProfielButtonPanel.add(labelPanel7);
 		maakProfielButtonPanel.add(maakProfielButton);
 		maakProfielButtonPanel.add(geenNieuwProfiel);
@@ -153,6 +210,7 @@ public class MaakProfielGUI implements ActionListener{
 	
 	public JPanel maakProfielPanel(){
 		JPanel maakProfielPanel = new JPanel(new BorderLayout());
+		maakProfielPanel.setBackground(Color.GRAY);
 		maakProfielPanel.add(labelPanel(), BorderLayout.WEST);
 		maakProfielPanel.add(textFieldPanel(), BorderLayout.CENTER);
 		maakProfielPanel.add(maakProfielButtonPanel(), BorderLayout.SOUTH);
@@ -161,8 +219,9 @@ public class MaakProfielGUI implements ActionListener{
 	
 	public JPanel maakProfielPanel1(){
 		JPanel maakProfielPanel1 = new JPanel();
+		maakProfielPanel1.setBackground(Color.GRAY);
 		maakProfielPanel1.add(maakProfielPanel());
-		maakProfielPanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Maak chat profiel aan:"));
+		maakProfielPanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "<html> <font color='black'>Maak chatprofiel aan: </font></html>"));
 		
 		return maakProfielPanel1;
 	}
@@ -170,7 +229,7 @@ public class MaakProfielGUI implements ActionListener{
 	public JFrame maakProfielFrame(){
 		maakProfielFrame = new JFrame("Maak nieuw Profiel");
 		maakProfielFrame.addWindowListener(listener);
-		maakProfielFrame.setSize(360,340);
+		maakProfielFrame.setSize(360,350);
 		maakProfielFrame.setVisible(true);
 		maakProfielFrame.setResizable(false);
 		maakProfielFrame.setLocationRelativeTo(null);
