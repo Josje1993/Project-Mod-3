aa3390f2ff5b0d9c179d4ab1eb67a2f4b72137f4
Thommie94boy <t.raaijen@live.nl>
2014-04-11 13:22:24 +0200

    textfields zijn rond

--- Intergrated Project/src/gui/ConnectGUI.java
+++ Intergrated Project/src/gui/ConnectGUI.java
@@ -16,6 +16,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
+import utils.RoundJTextField;
+
 /**
  * 
  */
@@ -66,7 +68,7 @@ public class ConnectGUI extends JPanel implements ActionListener {
 	}
 	
 	private JPanel nickName() {
-		nickName = new JTextField(nickNameString, 10);
+		nickName = new RoundJTextField(nickNameString, 10);
 		nickName.setForeground(Color.WHITE);
 		nickName.setBackground(Color.GRAY);
 		nickName.setEditable(true);
@@ -77,11 +79,11 @@ public class ConnectGUI extends JPanel implements ActionListener {
 	}
 	
 	private JPanel connectAddr() {
-		connAddr = new JTextField("Connect Address", 10);
+		connAddr = new RoundJTextField("Connect Address", 10);
 		connAddr.setForeground(Color.WHITE);
 		connAddr.setBackground(Color.GRAY);
 		connAddr.setEditable(true);
-		port = new JTextField("Port Address", 10);
+		port = new RoundJTextField("Port Address", 10);
 		port.setForeground(Color.WHITE);
 		port.setBackground(Color.GRAY);
 		connectAddrPanel = new JPanel();
