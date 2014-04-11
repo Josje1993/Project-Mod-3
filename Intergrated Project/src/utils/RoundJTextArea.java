2fa3a209659b572ed90d51e7f74ca2247b8f7f65
GerwinP <gerwinputtenstein@gmail.com>
2014-04-11 13:17:59 +0200

    Made hoekjes ronder

--- Intergrated Project/src/utils/RoundJTextArea.java
+++ Intergrated Project/src/utils/RoundJTextArea.java
@@ -18,18 +18,18 @@ public class RoundJTextArea extends JTextArea{
 	
 	protected void paintComponent(Graphics g){
 		g.setColor(getBackground());
-		g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+		g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
 		super.paintComponent(g);
 	}
 	
 	protected void paintBorder(Graphics g){
 		g.setColor(getForeground());
-		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
 	}
 	
 	public boolean contains(int x, int y){
 		if( shape == null || !shape.getBounds().equals(getBounds())){
-			shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+			shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 20, 20);
 		}
 		
 		return shape.contains(x, y);
