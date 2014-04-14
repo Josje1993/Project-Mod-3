9e5f0e7f309132e5e0ab1d74bedbb83086557014
Josje1993 <josje-vt-padje@hotmail.com>
2014-04-14 14:29:37 +0200

    suppressWarning toegevoegd

--- Intergrated Project/src/security/AESencrp.java
+++ Intergrated Project/src/security/AESencrp.java
@@ -20,7 +20,8 @@ public static String encrypt(String Data) throws Exception {
         Cipher c = Cipher.getInstance(ALGO);
         c.init(Cipher.ENCRYPT_MODE, key);
         byte[] encVal = c.doFinal(Data.getBytes());
-        String encryptedValue = new BASE64Encoder().encode(encVal);
+        @SuppressWarnings("restriction")
+		String encryptedValue = new BASE64Encoder().encode(encVal);
         return encryptedValue;
     }
 
@@ -28,7 +29,8 @@ public static String encrypt(String Data) throws Exception {
         Key key = generateKey();
         Cipher c = Cipher.getInstance(ALGO);
         c.init(Cipher.DECRYPT_MODE, key);
-        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        @SuppressWarnings("restriction")
+		byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
         byte[] decValue = c.doFinal(decordedValue);
         String decryptedValue = new String(decValue);
         return decryptedValue;
